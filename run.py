b5c788fe3ca0680e961a6bf1174b478e4fad1fba
Oksana0020 <>
2024-02-13 23:34:43 +0000

    create masked game board

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class TreasureHunt:
+    def __init__(self):
+        """
+        Represents the game board and masked board available when game is on
+        """
+        self.board = [" " for _ in range(100)]
+        self.masked_board = ["□" for _ in range(100)]  
+        self.game_on = True
+
+    def initialize_board(self):
+        """Initialize the game board."""
+        for i in range(100):
+            self.board[i] = "□"
+
+    def display_board(self):
+        """Display the game board."""
+        for i in range(0, len(self.board), 10):
+            line = ' '.join([f'{cell:<2}' for cell in self.board[i:i + 10]])
+            print(line)
+
+
+game = TreasureHunt()
+game.initialize_board()
+print("First Game Board:")
+game.display_board()
+
